d43b78753964372cf535bfb867d3ae3f34a11a5f
jialii <jx1150@nyu.edu>
2022-09-28 11:50:58 -0400

    db data inserts

--- db/db.py
+++ db/db.py
@@ -3,7 +3,26 @@ This file will manage interactions with our data store.
 At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
+import pymongo
+from pymongo import MongoClient
 
+def dataInsert():
+    CONNECTION_STRING = "mongodb+srv://jialii:Xujiali1@cluster0.wnpabny.mongodb.net/Ingredients"
+    client = MongoClient(CONNECTION_STRING)
+    #db = client.Ingredients
+    #coll = db.Sushi
+    docs = [
+        {"name": "Rice", "price": 7.0}, #set bread to $7 since the burger costs $7
+        {"name": "Zucchini", "price": 0.5},
+        {"name": "Avocado", "price": 1.0},
+        {"name": "Shrimp", "price": 1.0},
+        {"name": "Broccoli", "price": 0.5},
+        {"name": "Chicken", "price": 1.0},
+        {"name": "Salmon", "price": 1.0},
+        {"name": "Tuna", "price": 1.0},
+        ]
+
+    client.Ingredients.Sushi.insert_many(docs) #change collection name
 
 def fetch_pets():
     """
