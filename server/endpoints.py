4d27d1f48e34172b2083ec445eb0b717ac6bf37b
Zhenghua_Cao <zcao1531@gmail.com>
2022-09-29 22:40:26 -0400

    Andrew 9/29 class notes

--- server/endpoints.py
+++ server/endpoints.py
@@ -2,9 +2,13 @@
 This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
+from http import HTTPStatus
 
 from flask import Flask
 from flask_restx import Resource, Api
+import werkzeug.exceptions as wz
+
+import db.char_types as ctyp
 
 # import db.db as db
 
@@ -12,12 +16,14 @@ app = Flask(__name__)
 api = Api(app)
 
 LIST = 'list'
+DETAILS = 'details'
+MAIN_MENU = '/main_menu'
+MAIN_MENU_NM = 'Main Menu'
 HELLO = '/hello'
 MESSAGE = 'message'
 CHAR_TYPE_LIST = f'/character_types/{LIST}'
 CHAR_TYPE_LIST_NM = 'character_types_list'
-A_CHAR_TYPE = 'Wizard'
-ANTHOR_CHAR_TYPE = 'Warrior'
+CHAR_TYPE_DETAILS = f'/character_types/{DETAILS}'
 
 
 @api.route(HELLO)
@@ -34,16 +40,46 @@ class HelloWorld(Resource):
         return {MESSAGE: 'hello world'}
 
 
-@api.route(CHAR_TYPE_LIST)
-class CharacterTypeList(Resource):
+@api.route(MAIN_MENU)
+class MainMenu(Resource):
     """
-    This will get a list of character types
+    This will deliver our main menu.
     """
     def get(self):
         """
-        Returns a list of character types
+        Gets the main game menu.
         """
-        return {CHAR_TYPE_LIST_NM: [A_CHAR_TYPE, ANTHOR_CHAR_TYPE]}
+        return {MAIN_MENU_NM: {'the': 'menu'}}
+
+
+@api.route(CHAR_TYPE_LIST)
+class CharacterTypeList(Resource):
+    """
+    This will get a list of character types.
+    """
+    def get(self):
+        """
+        Returns a list of character types.
+        """
+        return {CHAR_TYPE_LIST_NM: ctyp.get_char_types()}
+
+
+@api.route(f'{CHAR_TYPE_DETAILS}/<char_type>')
+class CharacterTypeDetails(Resource):
+    """
+    This will get a list of character types.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def get(self, char_type):
+        """
+        Returns a list of character types.
+        """
+        ct = ctyp.get_char_type_details(char_type)
+        if ct is not None:
+            return {char_type: ctyp.get_char_type_details(char_type)}
+        else:
+            raise wz.NotFound(f'{char_type} not found.')
 
 
 @api.route('/endpoints')
--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -1,34 +1,46 @@
-
 import pytest
 
+import sys
+
+sys.path.append("..")
 import server.endpoints as ep
 
 TEST_CLIENT = ep.app.test_client()
 
+TEST_CHAR_TYPE = 'Warrior'
+
+
 def test_hello():
     """
-    See if Hello works
-    :return:
+    See if Hello works.
     """
     resp_json = TEST_CLIENT.get(ep.HELLO).get_json()
     assert isinstance(resp_json[ep.MESSAGE], str)
 
+
 def test_get_character_type_list():
     """
-    See if we can get a character type list properly
+    See if we can get a charcter type list properly.
     Return should look like:
         {CHAR_TYPE_LIST_NM: [list of chars types...]}
-    :return:
     """
     resp_json = TEST_CLIENT.get(ep.CHAR_TYPE_LIST).get_json()
     assert isinstance(resp_json[ep.CHAR_TYPE_LIST_NM], list)
 
-def test_get_character_type_list_non_empty():
+
+def test_get_character_type_list_not_empty():
     """
-    See if we can get a character type list properly
+    See if we can get a charcter type list properly.
     Return should look like:
         {CHAR_TYPE_LIST_NM: [list of chars types...]}
-    :return:
     """
     resp_json = TEST_CLIENT.get(ep.CHAR_TYPE_LIST).get_json()
     assert len(resp_json[ep.CHAR_TYPE_LIST_NM]) > 0
+
+
+def test_get_character_type_details():
+    """
+    """
+    resp_json = TEST_CLIENT.get(f'{ep.CHAR_TYPE_DETAILS}/{TEST_CHAR_TYPE}').get_json()
+    assert TEST_CHAR_TYPE in resp_json
+    assert isinstance(resp_json[TEST_CHAR_TYPE], dict)
