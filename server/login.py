4ddf790f946ed078b1cda3c11f824fc1c47884bf
Zhenghua_Cao <zcao1531@gmail.com>
2022-10-14 18:59:30 -0400

    temp login

--- server/login.py
+++ server/login.py
@@ -2,12 +2,13 @@ from pymongo import MongoClient
 # from bson.json_util import dumps
 # import random
 import json
-from flask import Flask, render_template, request, session, url_for, redirect, jsonify
+from flask import Flask, render_template, request
+# session, url_for, redirect
 
 app = Flask(__name__)
 
-# CONNECTION_STRING = "mongodb+srv://jialii:Xujiali1@\
-# cluster0.wnpabny.mongodb.net/Ingredients"
+CONNECTION_STRING = "mongodb+srv://jialii:Xujiali1@\
+cluster0.wnpabny.mongodb.net/Ingredients"
 #
 #
 # client = MongoClient(CONNECTION_STRING)
@@ -31,10 +32,31 @@ def home():
 @app.route('/loginAuth', methods=["POST"])
 def login():
     username = request.form['username']
-    print(username)
-    print('here')
-    return render_template('home.html')
-    # return redirect(url_for('home'))
+    client = MongoClient(CONNECTION_STRING)
+    my_db = client["Users"]
+    my_col = my_db["Users"]
+    data = my_col.find()
+    insert = True
+    if data is None:
+        u_id = 0
+    else:
+        u_id = 0
+        for user in data:
+            if user['name'] == username:
+                insert = False
+                break
+            u_id = max(u_id, user['u_id'])
+    if insert:
+        print("here1")
+        document = ({'u_id': u_id+1, 'name': username})
+        my_col.insert_one(document)
+        print(username)
+        print('here2')
+        return render_template('home.html')
+        # return redirect(url_for('home'))
+    else:
+        print("name is already used")
+        return render_template('login.html')
 
 
 def main():
