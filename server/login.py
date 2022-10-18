4777b506dee069864c5e4b1508e6c257a1394884
Zhenghua_Cao <zcao1531@gmail.com>
2022-10-18 17:37:06 -0400

    login jump to home with ingredients generated

--- server/login.py
+++ server/login.py
@@ -1,8 +1,9 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 # from bson.json_util import dumps
 # import random
 # import json
 from flask import Flask, render_template, request
+import ingredients_generator as ig
 # session, url_for, redirect
 
 app = Flask(__name__)
@@ -31,28 +32,36 @@ def home():
 
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def login_auth():
+    """
+    These comments are used to connect db
+    """
     username = request.form['username']
-    client = MongoClient(CONNECTION_STRING)
-    my_db = client["Users"]
-    my_col = my_db["Users"]
-    data = my_col.find()
+    # client = MongoClient(CONNECTION_STRING)
+    # my_db = client["Users"]
+    # my_col = my_db["Users"]
+    # data = my_col.find()
     insert = True
-    if data is None:
-        u_id = 0
-    else:
-        u_id = 0
-        for user in data:
-            if user['name'] == username:
-                insert = False
-                break
-            u_id = max(u_id, user['u_id'])
+    # if data is None:
+    #     u_id = 0
+    # else:
+    #     u_id = 0
+    #     for user in data:
+    #         if user['name'] == username:
+    #             insert = False
+    #             break
+    #         u_id = max(u_id, user['u_id'])
     if insert:
         print("here1")
-        document = ({'u_id': u_id+1, 'name': username})
-        my_col.insert_one(document)
+        """
+        These comments are used to connect db
+        """
+        # document = ({'u_id': u_id+1, 'name': username})
+        # my_col.insert_one(document)
         print(username)
         print('here2')
-        return render_template('home.html')
+
+        # return render_template('home.html')
+        return ig.generator()
         # return redirect(url_for('home'))
     else:
         error = "name is already used"
