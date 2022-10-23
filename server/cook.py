01afd2824b4acff9d4e5f60f28b7c1a65566f95a
jialii <jx1150@nyu.edu>
2022-10-23 10:52:40 -0400

    update

--- server/cook.py
+++ server/cook.py
@@ -1,4 +1,3 @@
-from pymongo import MongoClient
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -6,17 +5,15 @@ app = Flask(__name__)
 CONNECTION_STRING = "mongodb+srv://jialii:Xujiali1@\
 cluster0.wnpabny.mongodb.net/Ingredients"
 
-@app.route('/')
+@app.route('/cook')
 def index():
     return render_template('cook.html')
 
-
-@app.route('/cook')
+@app.route('/cookSuccess')
 def home():
     return render_template('success.html')
 
-
-@app.route('/cook')
+@app.route('/cookFailed')
 def home():
     return render_template('fail.html')
 
