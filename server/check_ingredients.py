71e4e04d1efde80ca145428b81ad0938b22f2e8e
tl3119 <tl3119@nyu.edu>
2022-11-06 10:21:00 -0500

    temp update

--- server/check_ingredients.py
+++ server/check_ingredients.py
@@ -1,19 +1,19 @@
-#from pymongo import MongoClient
-#from bson.json_util import dumps, loads
-#from flask import Flask, render_template
+# from pymongo import MongoClient
+# from bson.json_util import dumps, loads
+# from flask import Flask, render_template
 #
-#app = Flask(__name__)
+# app = Flask(__name__)
 #
-#dishes = ["Burger", "Pizza", "PokeBowl", "Salad", "Sushi"]
-#CONNECTION_STRING = "mongodb+srv://jialii:Xujiali1@\
-#cluster0.wnpabny.mongodb.net/Ingredients"
+# dishes = ["Burger", "Pizza", "PokeBowl", "Salad", "Sushi"]
+# CONNECTION_STRING = "mongodb+srv://jialii:Xujiali1@\
+# cluster0.wnpabny.mongodb.net/Ingredients"
 #
 #
 # def receive(ing_dict):
 #     pass
 #
 
-#def check_ingredients(ing_dict):
+# def check_ingredients(ing_dict):
 #   if dishes == "Burger":
 #        return ing_dict == "Bread&Meat"
 #    if dishes == "Pizza":
@@ -25,3 +25,4 @@
 #    if dishes == "Sushi":
 #        return ing_dict == "Rice"
 #    return False
+#    
