8b0b911773702708917da1f51aa4705a22376665
tl3119 <tl3119@nyu.edu>
2022-11-06 10:14:13 -0500

    Update check_ingredients and style.css

--- server/check_ingredients.py
+++ server/check_ingredients.py
@@ -1,27 +1,27 @@
 from pymongo import MongoClient
-from bson.json_util import dumps, loads
-from flask import Flask, render_template
+#from bson.json_util import dumps, loads
+#from flask import Flask, render_template
 #
-app = Flask(__name__)
+#app = Flask(__name__)
 #
-dishes = ["Burger", "Pizza", "PokeBowl", "Salad", "Sushi"]
-CONNECTION_STRING = "mongodb+srv://jialii:Xujiali1@\
-cluster0.wnpabny.mongodb.net/Ingredients"
+#dishes = ["Burger", "Pizza", "PokeBowl", "Salad", "Sushi"]
+#CONNECTION_STRING = "mongodb+srv://jialii:Xujiali1@\
+#cluster0.wnpabny.mongodb.net/Ingredients"
 #
 #
 # def receive(ing_dict):
 #     pass
 #
 
-def check_ingredients(ing_dict):
-    if dishes == "Burger":
-        return ing_dict == "Bread&Meat"
-    if dishes == "Pizza":
-        return ing_dict == "Crust"
-    if dishes == "PokeBowl":
-        return ing_dict == "RiceBowl"
-    if dishes == "Salad":
-        return ing_dict == "Salad"
-    if dishes == "Sushi":
-        return ing_dict == "Rice"
-    return False
+#def check_ingredients(ing_dict):
+#   if dishes == "Burger":
+#        return ing_dict == "Bread&Meat"
+#    if dishes == "Pizza":
+#        return ing_dict == "Crust"
+#    if dishes == "PokeBowl":
+#        return ing_dict == "RiceBowl"
+#    if dishes == "Salad":
+#        return ing_dict == "Salad"
+#    if dishes == "Sushi":
+#        return ing_dict == "Rice"
+#    return False
