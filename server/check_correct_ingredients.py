b003c659881b4138a89b597dc343b7ad0db5678f
Zhenghua_Cao <zcao1531@gmail.com>
2022-11-07 12:10:51 -0500

    added check_correct_ingredients.py and tests no db connection yet

--- server/check_correct_ingredients.py
+++ server/check_correct_ingredients.py
@@ -1,7 +1,41 @@
-# from bson.json_util import dumps, loads
-# from ingredients_generator import random_ingredients
-#
-#
-#
-# def check_correct_ingredients(ret):
-#
+# list of lists, including uid and ing:price dictionary
+orders_tb = [["Test User", {"Salad": 6, "Broccoli": 0.5, "Tuna": 1}],
+             ["Test User", {"RiceBowl": 7, "Shrimp": 1, "Broccoli": 0.5}],
+             ["Bowen", {"Salad": 6, "Broccoli": 0.5, "Tuna": 1}]]
+
+# User's return from the front-end
+ret = ["Test User", {"Salad": 6, "Broccoli": 0.5, "Tuna": 1}]
+
+
+# check whether there is a order returned by the user in the order_tb
+def check_correct_ingredients(order):
+    length = len(orders_tb) - 1
+    uid = order[0]
+    cnt = 0
+    for item in orders_tb:
+        money = 0
+        end = True
+        if item[0] == uid:
+            if len(item[1]) == len(order[1]):
+                for food in order[1].keys():
+                    if food not in item[1]:
+                        end = False
+                        break
+                    else:
+                        money += order[1][food]
+                if end:
+                    orders_tb[cnt], orders_tb[length] = \
+                        orders_tb[length], orders_tb[cnt]
+                    orders_tb.pop()
+                    return float(money)
+        cnt += 1
+    return -1.0
+
+
+def main():
+    money = check_correct_ingredients(ret)
+    print(money)
+
+
+if __name__ == "__main__":
+    main()
---
+++ server/tests/test_check_correct_ingredients.py
@@ -0,0 +1,22 @@
+import server.check_correct_ingredients as cci
+
+# list of lists, including uid and ing:price dictionary
+orders_tb = [["Test User", {"Salad": 6, "Broccoli": 0.5, "Tuna": 1}],
+      ["Test User", {"RiceBowl": 7, "Shrimp": 1, "Broccoli": 0.5}],
+      ["Bowen", {"Salad": 6, "Broccoli": 0.5, "Tuna": 1}]]
+
+# User's return from the front-end
+ret_1 = ["Test User", {"Salad": 6, "Broccoli": 0.5, "Tuna": 1}]
+ret_2 = ["Test User", {"Salad": 6, "Tuna": 1}]
+
+
+def test_check_correct_ingredients_correct():
+    price = cci.check_correct_ingredients(ret_1)
+    assert price == 7.5
+    assert isinstance(price, float)
+
+
+def test_check_correct_ingredients_incorrect():
+    price = cci.check_correct_ingredients(ret_2)
+    assert price == -1.0
+    assert isinstance(price, float)
