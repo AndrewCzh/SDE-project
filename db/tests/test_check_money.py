3e7e65699169dfccfa831a33056154e833d44409
Zhenghua_Cao <zcao1531@gmail.com>
2022-11-30 19:35:49 -0500

    add test_change_money_type in db/tests/test_check_money.py

--- db/tests/test_check_money.py
+++ db/tests/test_check_money.py
@@ -14,6 +14,14 @@ def test_check_money():
     ret = cm.check_money()
     assert isinstance(ret, bool)
 
+
+@pytest.mark.skip()
+def test_change_money_type():
+    cm.change_money_types()
+    ct = cm.get_money_types()
+    assert isinstance(ct, float)
+
+
 @pytest.mark.skip()
 def test_del_money_type():
     pass
