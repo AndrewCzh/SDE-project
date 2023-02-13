b20d448bc19cc101a3ec3bd33a1dc94220fa13dc
Zhenghua_Cao <zcao1531@gmail.com>
2023-02-13 13:49:58 -0500

    created a game collection in mongo db

---
+++ db/games.py
@@ -0,0 +1,40 @@
+"""
+This module encapsulates details about games.
+"""
+from pymongo import MongoClient
+# from bson.json_util import dumps
+import uuid
+import db.db_connect as dbc
+
+COLLECTION = "Games"
+DB = "Games"
+GAME = 'game'
+# TEST_GAME_NAME = 'Test game'
+# NUM_PLAYERS = 'num_players'
+
+dbc.connect_db()
+
+
+def get_games():
+    user_ls = dbc.fetch_all(COLLECTION, DB)
+    return user_ls
+
+
+def get_game_details(game):
+    filt = {GAME: game}
+    info = dbc.fetch_one(COLLECTION, DB, filt)
+    if info is not None:
+        return info
+    else:
+        raise ValueError('Game does not exist.')
+
+
+def main():
+    pass
+    # game = get_games()
+    # print(f'{game=}')
+    # print(f'{get_game_details(TEST_GAME_NAME)}')
+
+
+if __name__ == '__main__':
+    main()
