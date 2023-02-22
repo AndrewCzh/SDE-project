4d300afda4e0c8f7a11c23e615fa4f7ea02e05a3
AndrewCzh <91338259+AndrewCzh@users.noreply.github.com>
2023-02-22 14:20:37 -0500

    Update deploy.sh
    fixing rebuild.sh no such file

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ then
 fi
 
 echo "SSHing to PythonAnywhere."
-sshpass -p $SDE_PA_PWD ssh -tt $PA_USER@ssh.pythonanywhere.com << EOF
+sshpass -p $SDE_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
     cd SDE-project; ./rebuild.sh
 EOF
