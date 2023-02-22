2fb41d78091d7dc599d8a6bdf53ce12aa136c27e
jialii <84836069+jialii@users.noreply.github.com>
2023-02-22 14:06:39 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This shell script deploys a new version to a server.
 
-ssh -tt
 export PA_USER=Andrew1531
 
 if [ -z "$SDE_PA_PWD" ]
@@ -11,6 +10,6 @@ then
 fi
 
 echo "SSHing to PythonAnywhere."
-sshpass -p $SDE_PA_PWD ssh -tt "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
+sshpass -p $SDE_PA_PWD ssh -t -t "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
     cd SDE-project; ./rebuild.sh
 EOF
