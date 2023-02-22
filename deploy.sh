1f11afe1165ece293bb1506e8be9b3eb3b71ec85
jialii <84836069+jialii@users.noreply.github.com>
2023-02-22 18:27:19 -0500

    fixing auto-deploy

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ fi
 echo "SSHing to PythonAnywhere."
 sshpass -p $SDE_PA_PWD ssh -tt $PA_USER@ssh.pythonanywhere.com << EOF
 sshpass -p $SDE_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
-    cd SDE-project; rebuild.sh
+    cd SDE-project; ./rebuild.sh
 EOF
