f9e533c8359ed5f825ced76a13da64f289d16042
SanyuFeng <89467921+SanyuFeng@users.noreply.github.com>
2023-03-05 22:48:01 -0500

    connect tool to database and add tests

--- server/check_tool.py
+++ server/check_tool.py
@@ -18,7 +18,27 @@ cluster0.wnpabny.mongodb.net/cooktool"
 #              ["Salad", "Lettuce"]]
 #     money = check_correct_ingredients(order)
 #     print(money)
+# Define MongoDB connection parameters
+
+
+client = MongoClient('localhost', 27017)
+db = client['cooktool']
+
+# Define function to check if a tool exists in a collection
+def check_tool(collection_name, tool_name):
+    collection = db[collection_name]
+    tool = collection.find_one({"name": tool_name})
+    if tool is None:
+        print("The tool {} does not exist in the collection {}".format(tool_name, collection_name))
+    else:
+        print("The tool {} exists in the collection {}".format(tool_name, collection_name))
+
+# Test function with a wrong tool name
+def test_wrong_tool():
+    check_tool("measuring_cups", "potato masher")
+
+# Connect to the database and call the test function
 
 
 if __name__ == "__main__":
-    main()
+    test_wrong_tool()
