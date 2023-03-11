a04efd9f032f5e6e1c149c4f0396937039d466b5
SanyuFeng <89467921+SanyuFeng@users.noreply.github.com>
2023-03-10 21:17:04 -0500

    test tool type

--- server/tests/test_cook.py
+++ server/tests/test_cook.py
@@ -3,3 +3,7 @@ from bson.json_util import loads
 
 sys.path.append("..")
 import server.cook as ck
+
+# Test function with a wrong tool name
+def test_wrong_tool():
+    check_tool("measuring_cups", "potato masher")
