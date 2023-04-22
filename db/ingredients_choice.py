09ad1b6e35040d30a4968deaddb615c928328b2c
SanyuFeng <89467921+SanyuFeng@users.noreply.github.com>
2023-04-22 17:00:49 -0400

    ingredients_choice

---
+++ db/ingredients_choice.py
@@ -0,0 +1,65 @@
+BURGER = 'burger_ingredients'
+PIZZA = 'pizza_ingredients'
+POKEBOWL = 'pokebowl_ingredients'
+SALAD = 'salad_ingredients'
+SUSHI = 'sushi_ingredients'
+
+Ingredient_Choices = {
+    BURGER:{
+        'Bread': 7,
+        'Cheese': 1,
+        'Tomato': 0.5,
+        'Lettuce': 0.5,
+        'Grilled Mushroom': 0.5,
+        "Pickles": 0.5,
+        'Onions': 0.5,
+        'Mushroom': 0.5,
+        'Bacon': 1.5,
+        'Beef': 3,
+    },
+    PIZZA:{
+        'Green Peppers': 0.5,
+        'Spinach':0.5,
+        'Pepperoni': 1.5,
+        'Crust': 8,
+        'Red peppers':0.5,
+        'Mushroom': 0.5,
+        'Cheese':1,
+        'Pineapple':1,
+        'Tomato':0.5,
+        'Onions':0.5,
+
+    },
+    POKEBOWL:{
+        'Tuna':1,
+        'Zucchini':0.5,
+        'Avocado':1,
+        'Shrimp':1,
+        "Chicken":1,
+        "Broccoli":0.5,
+        'Salmon':1,
+        'Rice':7,
+    },
+    SALAD:{
+        'Salad_Dressing':6,
+        'Lettuce':0.5,
+        "Avocado":1,
+        'Croutons':1,
+        'Broccoli':0.5,
+        'Salmon':1,
+        'Chicken':1,
+        'Tuna':1,
+        'Corn':1,
+    },
+    SUSHI:{
+        'Avocado':1,
+        'Zucchini':0.5,
+        "Rice":7,
+        'Shrimp':1,
+        "Broccoli":0.5,
+        'Chicken':1,
+        'Salmon':1,
+        'Tuna':1,
+    }
+
+}
